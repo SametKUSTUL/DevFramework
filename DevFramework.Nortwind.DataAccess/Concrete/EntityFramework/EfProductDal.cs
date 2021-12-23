7a70fd02a88aa8abb8f285d28568c387d79f24f0
Samet KUSTUL <47284751+SametKUSTUL@users.noreply.github.com>
2021-12-23 16:52:34 +0300

    EntityFramework Baglantisi Yapildi.

---
+++ DevFramework.Nortwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,17 @@
+﻿using DevFramework.Core.DataAccess.EntityFramework;
+using DevFramework.Northwind.Entities.Concrete;
+using DevFramework.Nortwind.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Nortwind.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal :EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
+    {
+      
+    }
+}
---
+++ DevFramework.Nortwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,26 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using DevFramework.Nortwind.DataAccess.Concrete.EntityFramework.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Nortwind.DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext:DbContext
+    {
+        public NorthwindContext()
+        {
+            Database.SetInitializer<NorthwindContext>(null); // veri tabanında olmayan bir tablo fiels varsa oluşturma.
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProductMap());
+        }
+
+    }
+}
