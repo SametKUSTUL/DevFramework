06162d8867f56b631c7c4f764a3ca2a103485efb
Samet KUSTUL <47284751+SametKUSTUL@users.noreply.github.com>
2021-12-23 14:30:54 +0300

    23.12.2021-1430

--- DevFramework.Core/DataAccess/IEntityRepository.cs
+++ DevFramework.Core/DataAccess/IEntityRepository.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using DevFramework.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DevFramework.Core.DataAccess
 {
-    public interface IEntityRepository
+    public interface IEntityRepository<T> where T:class, IEntity,new()
     {
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+
+        T Get(Expression<Func<T, bool>> filter = null);
+
+        T Add(T entity);
+
+        T Update(T entity);
+
+        T Delete(T entity);
+
+
     }
 }
